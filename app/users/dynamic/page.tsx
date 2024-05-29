57b06e7147b5a9227231cc7cd62460d5b640c6c5
ayroid <ayushsk0000@gmail.com>
2024-05-29 15:45:30 +0530

    feat: implement Daisy UI
    Implemented Daisy UI Table Component

--- app/users/dynamic/page.tsx
+++ app/users/dynamic/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 interface User {
   id: number;
   name: string;
+  email: string;
 }
 
 const DynamicUserPage = async () => {
@@ -14,11 +15,22 @@ const DynamicUserPage = async () => {
     <>
       <h1>Users</h1>
       <h2>{new Date().toLocaleString()}</h2>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
