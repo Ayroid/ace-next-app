0a190c08e05bab6363f12c304db18993eaf1c3e5
ayroid <ayushsk0000@gmail.com>
2024-05-30 23:49:19 +0530

    fix: check add for already existing product

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -14,11 +14,25 @@ export async function POST(request: NextRequest) {
   if (!validation.success) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
+
+  const product = await prisma.product.findFirst({
+    where: {
+      name: body.name,
+    },
+  });
+
+  if (product) {
+    return NextResponse.json(
+      { message: "Product with this name already exists" },
+      { status: 400 }
+    );
+  }
+
   const data = await prisma.product.create({
     data: {
       name: body.name,
       price: body.price,
     },
   });
-  return NextResponse.json(data);
+  return NextResponse.json(data, { status: 201 });
 }
