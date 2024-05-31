3c767cd5d5cd3a6b198f54890842b194cfcc3f5d
ayroid <ayushsk0000@gmail.com>
2024-06-01 00:47:15 +0530

    feat: style Mail using Tailwind

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -6,22 +6,25 @@ import {
   Text,
   Link,
   Preview,
+  Tailwind,
 } from "@react-email/components";
 
 const WelcomeTemplate = () => {
   return (
     <Html>
-      <Body>
-        <Preview>This is a preview of the email template.</Preview>
-        <Container>
-          <Text>Welcome to our app!</Text>
-          <Text>
-            We&apos;re excited to have you on board. Please let us know if you
-            have any questions.
-          </Text>
-          <Link href="https://google.com">Visit our website</Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        <Body className="bg-white font-mono">
+          <Preview>This is a preview of the email template.</Preview>
+          <Container>
+            <Text className="text-2xl font-bold">Welcome to our app!</Text>
+            <Text>
+              We&apos;re excited to have you on board. Please let us know if you
+              have any questions.
+            </Text>
+            <Link href="https://google.com">Visit our website</Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
 };
