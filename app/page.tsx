09a248006281be43199fbe835c5c5fd3da558775
ayroid <ayushsk0000@gmail.com>
2024-06-01 13:20:34 +0530

    feat: implement Image Optimization

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { getServerSession } from "next-auth";
 import Link from "next/link";
 import React from "react";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import Image from "next/image";
+import bgimage from "@/public/images/bgimage.jpg";
 
 const Home = async () => {
   const session = await getServerSession(authOptions);
@@ -17,6 +19,14 @@ const Home = async () => {
       <Link href={"/users/new"}>New Users</Link>
       <br />
       <Link href={"/admins"}>Admins</Link>
+      <br />
+      <Image
+        src={bgimage}
+        alt="BgImage"
+        quality={100}
+        className="object-cover"
+        priority
+      />
     </main>
   );
 };
